daca10ad3026fba4e4473f07d108b659aaf09ffe
demisforever <demisgero22@gmail.com>
2023-09-10 23:31:54 -0300

    validate that the book is not borrowed to borrow

--- app/interactors/book_state_create.rb
+++ app/interactors/book_state_create.rb
@@ -6,10 +6,17 @@ class BookStateCreate
     #context.fail!(error: Constants::BORROWED_BOOK) if context[:age] < 18 # si el libro ya esta prestado
   end
 
-  def call
-    state = State.create(context.state_params)     
-    context.fail! unless state.valid? 
-    context.state = state
+  def call    
+    # Check if a state already exists for the same book_id with returned_at in nil
+    existing_state = State.find_by(book_id: context.state_params[:book_id], returned_at: nil)
+
+    if existing_state
+      context.fail!(error: "The book is already borrowed, select another book or came back in 4 days.")
+    else
+      state = State.create(context.state_params)
+      context.fail! unless state.valid?
+      context.state = state
+    end
   end
 
   after do
